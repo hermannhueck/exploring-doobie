f00f50c35b70b294f08f4c83bc4b4f4c724baef0
Hermann Hueck <hermann.hueck@mailbox.org>
2022-06-02 23:34:03 +0200

    add source files for chap 14-17

---
+++ src/main/scala/book/_14managingconnections/HikariApp.scala
@@ -0,0 +1,40 @@
+// See 'book of doobie', chapter 14:
+// https://tpolecat.github.io/doobie/docs/14-Managing-Connections.html
+//
+package book._14managingconnections
+
+// import cats.effect.{ExitCode, IO, IOApp, Resource}
+import cats.effect.{ExitCode, IO, IOApp}
+
+// import doobie._
+// import doobie.hikari._
+// import doobie.implicits._
+
+object HikariApp extends IOApp {
+
+  // Resource yielding a transactor configured with a bounded connect EC and an unbounded
+  // transaction EC. Everything will be closed and shut down cleanly after use.
+  // val transactor: Resource[IO, HikariTransactor[IO]] =
+  //   for {
+  //     ce <- ExecutionContexts.fixedThreadPool[IO](32) // our connect EC
+  //     be <- Blocker[IO]                               // our blocking EC
+  //     xa <- HikariTransactor.newHikariTransactor[IO](
+  //             "org.h2.Driver",                      // driver classname
+  //             "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", // connect URL
+  //             "sa",                                 // username
+  //             "",                                   // password
+  //             ce,                                   // await connection here
+  //             be                                    // execute JDBC operations here
+  //           )
+  //   } yield xa
+
+  def run(args: List[String]): IO[ExitCode] =
+    // transactor.use { xa =>
+    //   // Construct and run your server here!
+    //   for {
+    //     n <- sql"select 42".query[Int].unique.transact(xa)
+    //     _ <- IO(println(n))
+    //   } yield ExitCode.Success
+    // }
+    ???
+}
---
+++ src/main/scala/book/_16extensionsh2/H2App.scala
@@ -0,0 +1,38 @@
+// See 'book of doobie', chapter 16:
+// https://tpolecat.github.io/doobie/docs/16-Extensions-H2.html
+//
+package book._16extensionsh2
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+// import doobie._
+// import doobie.h2._
+// import doobie.implicits._
+
+object H2App extends IOApp {
+
+  // Resource yielding a transactor configured with a bounded connect EC and an unbounded
+  // transaction EC. Everything will be closed and shut down cleanly after use.
+  // val transactor: Resource[IO, H2Transactor[IO]] =
+  //   for {
+  //     ec      <- ExecutionContexts.fixedThreadPool[IO](32) // our connect EC
+  //     blocker <- Blocker[IO]                               // our blocking EC
+  //     xa      <- H2Transactor.newH2Transactor[IO](
+  //                  "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", // connect URL
+  //                  "sa",                                 // username
+  //                  "",                                   // password
+  //                  ec,                                   // await connection here
+  //                  blocker                               // execute JDBC operations here
+  //                )
+  //   } yield xa
+
+  def run(args: List[String]): IO[ExitCode] =
+    // transactor.use { xa =>
+    //   // Construct and run your server here!
+    //   for {
+    //     n <- sql"select 42".query[Int].unique.transact(xa)
+    //     _ <- IO(println(n))
+    //   } yield ExitCode.Success
+    // }
+    ???
+}
