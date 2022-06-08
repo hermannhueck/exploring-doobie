a300d52501a573ab42eecbeb2357db61954faaf1
Hermann Hueck <hermann.hueck@mailbox.org>
2022-06-08 11:33:29 +0200

    work on HikariApp

--- src/main/scala/book/_14managingconnections/HikariApp.scala
+++ src/main/scala/book/_14managingconnections/HikariApp.scala
@@ -3,38 +3,38 @@
 //
 package book._14managingconnections
 
-// import cats.effect.{ExitCode, IO, IOApp, Resource}
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Resource}
+// import cats.effect.{ExitCode, IO, IOApp}
 
-// import doobie._
-// import doobie.hikari._
-// import doobie.implicits._
+import doobie._
+import doobie.hikari._
+import doobie.implicits._
 
 object HikariApp extends IOApp {
 
   // Resource yielding a transactor configured with a bounded connect EC and an unbounded
   // transaction EC. Everything will be closed and shut down cleanly after use.
-  // val transactor: Resource[IO, HikariTransactor[IO]] =
-  //   for {
-  //     ce <- ExecutionContexts.fixedThreadPool[IO](32) // our connect EC
-  //     be <- Blocker[IO]                               // our blocking EC
-  //     xa <- HikariTransactor.newHikariTransactor[IO](
-  //             "org.h2.Driver",                      // driver classname
-  //             "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", // connect URL
-  //             "sa",                                 // username
-  //             "",                                   // password
-  //             ce,                                   // await connection here
-  //             be                                    // execute JDBC operations here
-  //           )
-  //   } yield xa
+  val transactor: Resource[IO, HikariTransactor[IO]] =
+    for {
+      ce <- ExecutionContexts.fixedThreadPool[IO](32) // our connect EC
+      // be <- Blocker[IO]                               // our blocking EC (needed for CE2)
+      xa <- HikariTransactor.newHikariTransactor[IO](
+              "org.h2.Driver",                      // driver classname
+              "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", // connect URL
+              "sa",                                 // username
+              "",                                   // password
+              ce                                    // await connection here
+              // be                                    // execute JDBC operations here
+            )
+    } yield xa
 
   def run(args: List[String]): IO[ExitCode] =
-    // transactor.use { xa =>
-    //   // Construct and run your server here!
-    //   for {
-    //     n <- sql"select 42".query[Int].unique.transact(xa)
-    //     _ <- IO(println(n))
-    //   } yield ExitCode.Success
-    // }
-    ???
+    transactor.use { xa =>
+      // Construct and run your server here!
+      for {
+        _ <- IO.println(dash80.green)
+        n <- sql"select 42".query[Int].unique.transact(xa)
+        _ <- IO(println(n))
+      } yield ExitCode.Success
+    }
 }
